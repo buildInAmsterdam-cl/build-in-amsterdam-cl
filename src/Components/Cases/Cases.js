d9656a26edbfb983532e7dcf57287d61c216b66e
Karli Stevens <karli.liz.st@gmail.com>
2018-06-27 09:28:07 -0600

    map over cases

--- src/Components/Cases/Cases.js
+++ src/Components/Cases/Cases.js
@@ -1,87 +1,45 @@
 import React, { Component } from 'react';
 import './Cases.css';
+import axios from 'axios';
 
 export default class Cases extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            cases: []
+            casesData: []
         }
     }
 
-    // componentDidMount() {
-    // axios.get('/cases').then(case => {
-    // this.setState({ cases: case.data})
-    // })
-    // }
-
-    // cases & media
-
-    // showCases (users) {
-    //     if (users.auth_id) {
-    //         return (
-    //             <div>
-
-
-    //             </div>
-    //         )
-    //     } else {
-    //         return this.state.cases.map((cases, index) => {
-    //             return (
-    //                 <div key={index} className='case_slider'>
-    //                     <img src='http://www.bhmpics.com/wallpapers/little_pony_art-800x480.jpg'/>
-    //                 </div>
-    //             )
-    //         })
-    //     }
-    // }
-
-    showCases() {
-        return (
-            <figure>
-                <div className='case_item'>
-                    <p>Case 1</p>
-                    <div className='img_box'>
-                        <img src='http://www.buildinamsterdam.com/app/uploads/2018/04/Frans-HalsH-KoosBreukelCombi_Zoom.jpg' /> 
-                    </div>
-                   
-                </div>
-                <div className='case_item'>
-                    <p>Case 2</p>
-                    <img src='http://www.buildinamsterdam.com/app/uploads/2018/04/Frans-HalsH-KoosBreukelCombi_Zoom.jpg' />
-                </div>
-                <div className='case_item'>
-                    <p>Case 3</p>
-                    <img src='http://www.buildinamsterdam.com/app/uploads/2018/04/Frans-HalsH-KoosBreukelCombi_Zoom.jpg' />
-                </div>
-                <div className='case_item'>
-                    <p>Case 4</p>
-                    <img src='http://www.buildinamsterdam.com/app/uploads/2018/04/Frans-HalsH-KoosBreukelCombi_Zoom.jpg' />
-                </div>
-                <div className='case_item'>
-                    <p>Case 5</p>
-                    <img src='http://www.buildinamsterdam.com/app/uploads/2018/04/Frans-HalsH-KoosBreukelCombi_Zoom.jpg' />
-                </div>
-                <div className='case_item'>
-                    <p>Case 6</p>
-                    <img src='http://www.buildinamsterdam.com/app/uploads/2018/04/Frans-HalsH-KoosBreukelCombi_Zoom.jpg' />
-                </div>
-                <div className='case_item'>
-                    <p>Case 7</p>
-                    <img src='http://www.buildinamsterdam.com/app/uploads/2018/04/Frans-HalsH-KoosBreukelCombi_Zoom.jpg' />
-                </div>
-            </figure>
-        )
+    componentDidMount() {
+        axios.get('/cases').then(res => {
+            this.setState({
+                casesData: res.data
+            })
+        })
     }
 
     render() {
-        return (
-            <div className='case_parent'>
-                <div className='case_array'>
-                    {this.showCases()}
-                    {/* {this.state.cases ? this.showCases(this.state.cases): ''} */}
+        console.log(this.state.casesData, 'cases much?')
+
+        let casesSlideshow = this.state.casesData ? this.state.casesData.map((cases, i) => {
+            return (
+                <div key={i} className='content'>
+                    <div className='case_item'>
+                        <div className='images'>
+                            <img src={cases.background_url} />
+                        </div>
+                        <span className='case_header'>
+                            <h2>{cases.title}</h2>
+                            <p>{cases.subtitle}</p>
+                        </span>
+                    </div>
 
                 </div>
+            )
+        }) : ''
+        return (
+            <div className='case_parent'>
+                    {casesSlideshow}
             </div>
         )
     }
