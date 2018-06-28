b057d1aeb87435f6afd7eecf0576b6fe23db2226
Karli Stevens <karli.liz.st@gmail.com>
2018-06-28 14:49:13 -0600

    step change

---
+++ src/Components/CaseFooter/CaseFooter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BiALogo from './BiALogo/BiALogo';
+
+export default function CaseFooter() {
+    return (
+        <div className="footer">
+            <div className="footer-left">
+                <BiALogo />
+            </div>
+            <div className="footer-right">
+
+            </div>
+        </div>
+    )
+}
