f8b345d2a7db9a997768e0b043265b2445040343
jonbaxt <jonbaxt@gmail.com>
2018-06-28 12:12:40 -0600

    Where I'm at

--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import Cases from './Components/Cases/Cases';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
-
+import casedummy1 from './Components/CaseComponents/casedummy1';
+import casedummy2 from './Components/CaseComponents/casedummy2';
 export default (
     <Switch>
         <Route path='/' component={Cases} exact />
         <Route path='/about' component={About} />
         <Route path='/contact' component={Contact} />
+        <Route path='/case1' component={casedummy1} />
+        <Route path='/case2' component={casedummy2} />
     </Switch>
 )
