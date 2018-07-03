7c07e993e6f543166b6d6596736454d7d2940206
Holly Lyons <hollylyons@Hollys-MacBook-Pro.local>
2018-07-03 00:59:23 -0600

    hl-sections-styling

--- src/Components/Sections/ColorPicker.js
+++ src/Components/Sections/ColorPicker.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-// import './ColorPicker.css'
+import './ColorPicker.css'
 
-export default function ColorPicker(props){
-    
+export default function ColorPicker(props) {
+
     return (
-        <div className='colorPickerMain'> 
-        {/* colorpicker hardcoded image */}
-            <img className='colorPickerResize'  alt='colors' /> 
-            <h3>{props.caption1.caption_title}</h3>
-            <p>{props.caption1.caption_content}</p>
+        <div className='colorPickerMain'>
+            {/* colorpicker hardcoded image */}
+            <div><img className='colorPickerResize' alt='colors' /></div>
+            <div className='colorCaptionContainer'>
+                <div className='colorCaption'>
+                    <h3>{props.caption1.caption_title}</h3>
+                    <p>{props.caption1.caption_content}</p>
+                </div>
+            </div>
         </div>
     )
 }
