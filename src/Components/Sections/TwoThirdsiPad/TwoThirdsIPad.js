8678ba48932bc9a677e0d32b712599a5e4825c1e
Karli Stevens <karli.liz.st@gmail.com>
2018-07-03 15:24:24 -0600

    ipad shapes

--- src/Components/Sections/TwoThirdsiPad/TwoThirdsIPad.js
+++ src/Components/Sections/TwoThirdsiPad/TwoThirdsIPad.js
@@ -9,23 +9,21 @@ export default function TwoThirdsIPad(props) {
     return (
         <div className='twoThirdsiPadMain'>
             <div className='videoFrame'>
-                <iframe className='twoThirdsipadResize' src={props.media1.media_url} alt=''></iframe>
-
-                {/* <Vimeo 
-            
-                videoId={225432736}
-                autoplay={true}
-                $mountNode
-               /> */}
-
-
-
+                <div>
+                    <iframe className='twoThirdsipadResize' width="840" height="628" muted='1' autoplay='1' frameborder="1" allowfullscreen src={props.media1.media_url} alt=''></iframe>
+                    {/* <iframe>
+                    <Vimeo
+                        src={props.media1.media_url}
+                        videoId={225432736}
+                        autoplay={true}
+                        muted={true}
+                        $mountNode
+                    />
+                </iframe>
                 <oembed>
-                   <html/> <iframe src="https://player.vimeo.com/video/225432736?muted=0&autoplay=1" width="840" height="628" frameborder="10" allowfullscreen></iframe>
-
-
-                </oembed>
-
+                    <html /> <iframe src="https://player.vimeo.com/video/225432736?muted=0&autoplay=1" width="840" height="628" frameborder="10" allowfullscreen></iframe>
+                </oembed> */}
+                </div>
             </div>
         </div>
     )
