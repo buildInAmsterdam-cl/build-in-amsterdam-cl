c51c9719c39c744fce45af0dd0e4e7950fa03b25
jonbaxt <jonbaxt@gmail.com>
2018-07-04 09:47:15 -0600

    added to seedfile and made more stuff

--- src/Components/Sections/FullGrid6PicsInIcons/FullGrid6PicsInIcons.js
+++ src/Components/Sections/FullGrid6PicsInIcons/FullGrid6PicsInIcons.js
@@ -2,26 +2,26 @@ import React from 'react';
 import './FullGrid6PicsInIcons.css'
 
 export default function FullGrid6PicsInIcons(props) {
-
+    // console.log(props)
     return (
         <div className='FullGrid6PicsInIconsMain'>
             <div className='blocks'>
-                <img className='gridImg' src='' alt='' />
+                <img className='gridImg' src={props.media1.media_url} alt='' />
             </div>
             <div className='blocks'>
-                <img className='gridImg' src='' alt='' />
+                <img className='gridImg' src={props.media2.media_url} alt='' />
             </div>
             <div className='blocks'>
-                <img className='gridImg' src='' alt='' />
+                <img className='gridImg' src={props.media3.media_url} alt='' />
             </div>
             <div className='blocks'>
-                <img className='gridImg' src='' alt='' />
+                <img className='gridImg' src={props.media4.media_url} alt='' />
             </div>
             <div className='blocks'>
-                <img className='gridImg' src='' alt='' />
+                <img className='gridImg' src={props.media5.media_url} alt='' />
             </div>
             <div className='blocks'>
-                <img className='gridImg' src='' alt='' />
+                <img className='gridImg' src={props.media6.media_url} alt='' />
             </div>
         </div>
     )
