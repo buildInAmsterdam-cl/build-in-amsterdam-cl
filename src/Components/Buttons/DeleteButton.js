b238d5e9a9e6c0cad12f703d443f0f23f27c12cc
Karli Stevens <karli.liz.st@gmail.com>
2018-07-12 19:48:56 -0600

    toggles, styles, animations

--- src/Components/Buttons/DeleteButton.js
+++ src/Components/Buttons/DeleteButton.js
@@ -7,7 +7,7 @@ export default function Delete(props){
     console.log('clicked delete')
     return(
     <div>
-        <button style={{color: '#fff', fontFamily: 'circular, sans-serif', fontSize: '.7em', marginTop: '1em', letterSpacing:'.1em'}} onClick={() => Axios.delete(`/deletecase/${props.id}`).then(res => console.log(res))}>Delete</button>
+        <div style={{color: '#fff', fontFamily: 'circular, sans-serif', fontSize: '.7em', letterSpacing:'.1em', marginTop: '10px', marginRight: '12px'}} onClick={() => Axios.delete(`/deletecase/${props.id}`).then(res => console.log(res))}>Delete</div>
     </div>
     )
 }
